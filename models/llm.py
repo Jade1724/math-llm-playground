33113a4df02d0422c5b09b5d9a74c004324db84d
Jade1724 <haruka.i1724@gmail.com>
2025-05-08 22:20:39 +1200

    feat: input and output class

--- models/llm.py
+++ models/llm.py
@@ -5,11 +5,18 @@ class Input:
        self.year = year
        self.answer = answer
 
+    def get_prompt(self) -> str:
+        return f"""
+        Question: {self.questions}
+        Answer: {self.answer}
+        """
+
 
 class Output:
-    def __init__(self, score: int, generated: str, topic: str, techniques: list[str], year: int):
+    def __init__(self, score: int, reasoning: str, feedback: str, topic: str, techniques: list[str], year: int):
         self.score = score
-        self.reasoning = generated
+        self.reasoning = reasoning
+        self.feedback = feedback
         self.topic = topic
         self.techniques = techniques
         self.year = year
